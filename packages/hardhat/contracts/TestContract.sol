b4c7f8b6e5bd001935043ee1c8675f317b6cb312
Fernando Ribas <fribas84@gmail.com>
2023-08-09 17:05:38 -0300

    crearted hardhat project to mock smartcontract

---
+++ packages/hardhat/contracts/TestContract.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+/// @title TestContract
+/// @author Fernando Ribas - fribas84
+/// @notice Contract to mock some status to test Snaps
+
+import {FrontDoorStructs} from './DataModel.sol';
+
+contract TestContract {
+  constructor() {}
+}
